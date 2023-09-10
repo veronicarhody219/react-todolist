045fa567a756da58caa6c8592339a773a0f89140
Veronica-rhody-219 <ngocdiep219@gmail.com>
2023-09-10 18:22:02 +0700

    feat: upload all files and folders

---
+++ src/NewTodoform.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+export function NewTodoForm({onSubmit}) {
+  const [newItem, setNewItem] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+    onSubmit(newItem);
+    setNewItem("");
+  }
+  return (
+    <form action="" className="new-item-form" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          type="text"
+          id="item"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
+}
